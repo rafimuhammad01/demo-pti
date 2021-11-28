8872a4bc7d9d2c57082af0766390946d4cc617f7
Rafi Muhammad <rafi.muhammad@mekari.com>
2021-11-28 23:12:39 +0700

    endpoint for update and delete

--- book/views.py
+++ book/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer, BookDetailSerializer
+from rest_framework import status
 
 # Create your views here.
 
@@ -24,7 +25,7 @@ class BookList(APIView):
         )
 
         serializers = BookDetailSerializer(book)
-        return Response(serializers.data)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
 
 
 
@@ -42,3 +43,37 @@ class BookDetail(APIView):
             "message" : "get book success",
             "data" : serializers.data
         })
+
+    def put(self, request, id):
+        try :
+            book = Book.objects.get(id=id)
+            book.name = request.data['name']
+            book.author = request.data['author']
+            book.volume = request.data['volume']
+            book.total_page = request.data['total_page']
+            book.publisher = request.data['publisher']
+            book.published_date = request.data['published_date']
+            book.save()
+        except Book.DoesNotExist:
+             return Response({
+                "error" : "buku tidak ditemukan"
+            })
+
+        serializers = BookDetailSerializer(book)
+        return Response({
+            "status" : 201,
+            "message" : "update successfull",
+            "data" : serializers.data
+        }, status=status.HTTP_201_CREATED)
+
+
+    def delete(self, request, id) :
+        book = Book.objects.get(id=id)
+        book.delete()
+
+        return Response({
+            "status" : 201,
+            "message" : "delete success",
+        }, status=status.HTTP_201_CREATED)
+
+        
